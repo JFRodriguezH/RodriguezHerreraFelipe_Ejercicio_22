09867d825ba961ad52f3e90d7d4e85429ba3e892
JFRodriguezH <jf.rodriguezh@uniandes.edu.co>
2019-10-24 18:04:54 +0000

    Primera version del metodo y la prueba

---
+++ complejo.py
@@ -0,0 +1,32 @@
+import math 
+class Complejo():
+    def __init__(self, real, imaginario):
+        self.real = real
+        self.imaginario = imaginario
+        self.norma = math.sqrt(real*real + imaginario*imaginario)
+
+    def conjugado(self):
+        self.imaginario = -self.imaginario
+
+    def calcula_norma(self):
+        self.norma = math.sqrt(self.real**2 + self.imaginario**2)
+
+    def pow(self, n):
+        a = Complejo(self.real, self.imaginario)
+        self.calcula_norma()
+        r = self.norma ** n
+        if abs(self.real)<1E-10:
+            theta = math.pi/2.0
+        else:
+            theta = math.atan(self.imaginario/self.real)
+        a.real = r * math.cos(n * theta)
+        a.imaginario = r * math.sin(n * theta)
+        return a 
+    
+    def mult(self, b):
+        q = self.real
+        w = self.imaginario
+        e = b.real
+        r = b.imaginario
+        self.real = q*e-w*r
+        self.imaginario = q*r+w*e
